2e6b70333c66e8c2f720aa7ce7644a8ea5e8fb94
Unknown <dmitrii.personal@gmail.com>
2018-07-24 17:33:30 +0300

    v0.3

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(xFrameOptions())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('public'));  
 app.use(express.static('uploads'));
 
 
