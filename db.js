72656a80736d3b077efe6debdf415d37590af560
Unknown <dmitrii.personal@gmail.com>
2018-07-26 14:34:56 +0300

    v0.6

--- db.js
+++ db.js
@@ -54,40 +54,40 @@ sequelize.sync({ force: true }).then(() => {
   Setting.create({
     name: "delete_order_timer", value: "10",
   })
-  Gates.create({
-   id_:1, name: "W1",checked:true,
-  })
-  Gates.create({
-    id_:2, name: "W2",checked:true,
-  })
-  Playlist.create({
-    src:'/images/download.jpg',timer:5, checked:true, sort:1
-  })
-  Playlist.create({
-    src:'/videos/Google Pay- Camping Trip.mp4',timer:5, checked:true, sort:2
-  })
-  Playlist.create({
-    src:'http://mcdonalds.md/',timer:5, checked:true, sort:3
-  })
-  Playlist.create({
-    src:'/images/thumb-1920-411820.jpg',timer:5, checked:true, sort:4
-  })
+  // Gates.create({
+  //  id_:1, name: "W1",checked:true,
+  // })
+  // Gates.create({
+  //   id_:2, name: "W2",checked:true,
+  // })
+  // Playlist.create({
+  //   src:'/images/download.jpg',timer:5, checked:true, sort:1
+  // })
+  // Playlist.create({
+  //   src:'/videos/Google Pay- Camping Trip.mp4',timer:5, checked:true, sort:2
+  // })
+  // Playlist.create({
+  //   src:'http://mcdonalds.md/',timer:5, checked:true, sort:3
+  // })
+  // Playlist.create({
+  //   src:'/images/thumb-1920-411820.jpg',timer:5, checked:true, sort:4
+  // })
   
-  Playlist.create({
-    src:'https://www.andys.md/',timer:5, checked:true, sort:6
-  })
-  Playlist.create({
-    src:'/videos/Introducing Microsoft Surface Go.mp4',timer:5, checked:true, sort:5
-  })
-  Playlist.create({
-    src:'/images/ExoticHDWallpapers.jpg',timer:5, checked:true, sort:7
-  })
-  Playlist.create({
-    src:'/videos/Meet iPhone X — Apple.mp4',timer:5, checked:true, sort:8
-  })
-  Playlist.create({
-    src:'https://laplacinte.md/',timer:5, checked:true, sort:9
-   })
+  // Playlist.create({
+  //   src:'https://www.andys.md/',timer:5, checked:true, sort:6
+  // })
+  // Playlist.create({
+  //   src:'/videos/Introducing Microsoft Surface Go.mp4',timer:5, checked:true, sort:5
+  // })
+  // Playlist.create({
+  //   src:'/images/ExoticHDWallpapers.jpg',timer:5, checked:true, sort:7
+  // })
+  // Playlist.create({
+  //   src:'/videos/Meet iPhone X — Apple.mp4',timer:5, checked:true, sort:8
+  // })
+  // Playlist.create({
+  //   src:'https://laplacinte.md/',timer:5, checked:true, sort:9
+  //  })
 });
 
 module.exports = {
