bb90401f2bd4e5e6a454d2014d49ee63fdc1d9e0
Unknown <dmitrii.personal@gmail.com>
2018-08-06 10:45:56 +0300

    v0.9.5.1

--- public/js/get_orders.js
+++ public/js/get_orders.js
@@ -1,6 +1,6 @@
 
-var socket = io.connect("s2.formulapro.com:3000");
-var socket2 = io.connect("http://localhost:3000");
+//var socket = io.connect("s2.formulapro.com:3000");
+var socket2 = io();//.connect("http://localhost:3000");
 
 var timer;
 
@@ -17,31 +17,31 @@ var list_id = 0;
 
 document.getElementById("audio1").play();
 
-socket.on('chat', function (data) {
-    $$("marqueess").load("/marque/data");
-    $$("order_timer").load("/delete_order_timer/data");
-    timer = $$("order_timer").serialize();
+// socket.on('chat', function (data) {
+//     $$("marqueess").load("/marque/data");
+//     $$("order_timer").load("/delete_order_timer/data");
+//     timer = $$("order_timer").serialize();
 
-    list_id = list_id + 1;
-    webix.ajax().get("/sounds/order",function(text, data,){
-        data = data.json();  
-            console.log(parseInt(data.value)/100);
-   document.getElementById("audio1").volume = parseInt(data.value)/100;
+//     list_id = list_id + 1;
+//     webix.ajax().get("/sounds/order",function(text, data,){
+//         data = data.json();  
+//             console.log(parseInt(data.value)/100);
+//    document.getElementById("audio1").volume = parseInt(data.value)/100;
   
-  });
-  document.getElementById("audio1").load();
-    document.getElementById("audio1").play();
-   // document.getElementById("audio1").volume = 1;
-    if (data.win !=0 && data.win!=null){
-    order = ( data.message +"-"+ data.win ).toString();}
-    else {  order = (data.message).toString();}
-    if(order!=""){
-    $$("mylist").add({
-        id: list_id,  // adding  an item with two properties
-        title: order.toString()
-    }, 0);
-    remove_item(list_id);}
-});
+//   });
+//   document.getElementById("audio1").load();
+//     document.getElementById("audio1").play();
+//    // document.getElementById("audio1").volume = 1;
+//     if (data.win !=0 && data.win!=null){
+//     order = ( data.message +"-"+ data.win ).toString();}
+//     else {  order = (data.message).toString();}
+//     if(order!=""){
+//     $$("mylist").add({
+//         id: list_id,  // adding  an item with two properties
+//         title: order.toString()
+//     }, 0);
+//     remove_item(list_id);}
+// });
 socket2.on('chat', function (data) {
     $$("marqueess").load("/marque/data");
     $$("order_timer").load("/delete_order_timer/data");
--- public/js/send_orders.js
+++ public/js/send_orders.js
@@ -1,13 +1,13 @@
 
-var socket = io.connect("s2.formulapro.com:3000");
-var socket2 = io.connect("http://localhost:3000");
+//var socket = io.connect("s2.formulapro.com:3000");
+var socket2 = io();//.connect("http://localhost:3000");
 
 $$("send").attachEvent("onItemClick", function (id, e) {
   //alert($$('text').getValue())
-  socket.emit('chat', {
-    message: $$('text').getValue(),
-    win: win
-  });
+  // socket.emit('chat', {
+  //   message: $$('text').getValue(),
+  //   win: win
+  // });
   socket2.emit('chat', {
     message: $$('text').getValue(),
     win: win
